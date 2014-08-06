5e8c16642eba1dcff7c2bb1cf046fc70a1b96804
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-08-06 15:37:16 +0000

    Fixed the warning message for ol Matlab versions

--- API_MO/SOFAstart.m
+++ API_MO/SOFAstart.m
@@ -24,7 +24,7 @@ if exist('OCTAVE_VERSION','builtin')
 else
     % We're in Matlab
   if verLessThan('matlab','8')
-    warning('SOFA for Matlab version >= 8 (2012b) not tested. Use on your risk.');
+    warning('SOFA for Matlab version <= 8 (2012b) not tested. Use on your risk.');
   end
 end
 
