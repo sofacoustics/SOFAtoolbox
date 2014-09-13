6c94148792e015c89e9feb85df2b8cacf0703951
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-09-13 10:24:47 +0000

    Added an default output to SOFAinfo()

--- API_MO/SOFAinfo.m
+++ API_MO/SOFAinfo.m
@@ -35,7 +35,9 @@ switch Obj.GLOBAL_SOFAConventions
     fprintf('Number of azimuth angles:   % 5.0f\n',length(unique(Obj.SourcePosition(:,1))));
     fprintf('Number of elevation angles: % 5.0f\n',length(unique(Obj.SourcePosition(:,2))));
     fprintf('Number of radii:            % 5.0f\n',length(unique(Obj.SourcePosition(:,3))));
-    fprintf('Sampling Rate: %.0f %s\n',Obj.Data.SamplingRate,Obj.Data.SamplingRate_Units);
+    if isfield(Obj.Data,'SamplingRate')
+        fprintf('Sampling Rate: %.0f %s\n',Obj.Data.SamplingRate,Obj.Data.SamplingRate_Units);
+    end
     fprintf('Dummy head: %s\n',Obj.GLOBAL_ListenerShortName);
     fprintf('Loudspeaker: %s\n',Obj.GLOBAL_SourceDescription);
     if size(Obj.ListenerPosition,1)==1
@@ -59,7 +61,31 @@ switch Obj.GLOBAL_SOFAConventions
     fprintf('\n');
    
 %%    
-  case 'SingleRoomDRIR'    
   otherwise
-    error('SOFAConventions not supported');
-end
+    fprintf('\n');
+    fprintf('%s\n',Obj.GLOBAL_Title);
+    fprintf('%s\n',repmat('=',1,length(Obj.GLOBAL_Title)));
+    fprintf('\n');
+    fprintf('Mesurement done by %s.\n',Obj.GLOBAL_Organization);
+    fprintf('\n');
+    fprintf('Contact: %s\n',Obj.GLOBAL_AuthorContact);
+    fprintf('License: %s\n',Obj.GLOBAL_License);
+    fprintf('URL: %s\n',Obj.GLOBAL_Origin);
+    fprintf('Reference: %s\n',Obj.GLOBAL_References);
+    fprintf('\n');
+    fprintf('Measurement details:\n');
+    fprintf('--------------------\n');
+    fprintf('SOFA Convention: %s',Obj.GLOBAL_SOFAConventions);
+    fprintf('Listener: %s\n',Obj.GLOBAL_ListenerShortName);
+    fprintf('Source: %s\n',Obj.GLOBAL_SourceDescription);
+    fprintf('Number of source positions:   % 7.0f\n',length(unique(Obj.SourcePosition,'rows')));
+    fprintf('Number of source views:       % 7.0f\n',length(unique(Obj.SourceView,'rows')));
+    fprintf('Number of emitters:           % 7.0f\n',length(unique(Obj.EmitterPosition,'rows')));
+    fprintf('Number of listener positions: % 7.0f\n',length(unique(Obj.ListenerPosition,'rows')));
+    fprintf('Number of listener views:     % 7.0f\n',length(unique(Obj.ListenerView,'rows')));
+    fprintf('Number of receivers:          % 7.0f\n',length(unique(Obj.ReceiverPosition,'rows')));
+    if isfield(Obj.Data,'SamplingRate')
+        fprintf('Sampling Rate: %.0f %s\n',Obj.Data.SamplingRate,Obj.Data.SamplingRate_Units);
+    end
+    fprintf('\n');
+ end
