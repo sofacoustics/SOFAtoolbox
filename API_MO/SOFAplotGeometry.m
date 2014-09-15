6b56907c06ce4683fff0006dab932b948b28d05e
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-09-15 13:42:30 +0000

    Updated SOFAplotGeometry() to work w and wo rooms

--- API_MO/SOFAplotGeometry.m
+++ API_MO/SOFAplotGeometry.m
@@ -18,83 +18,66 @@ end
 
 switch Obj.GLOBAL_SOFAConventions
 %%
-  case 'SimpleFreeFieldHRIR'
-    figure; hold on;
-    h=[];
-    OE=SOFAexpand(Obj);
-      % Listener: determine unique listener geometries
-    LP=OE.ListenerPosition(index,:); 
-    LV=OE.ListenerView(index,:); 
-    LU=OE.ListenerUp(index,:);
-    x=unique([LP LV LU],'rows');
-      % Listener: plot first appearance
-    h(end+1)=plot3(x(1,1), x(1,2), x(1,3),'ro','MarkerFaceColor',[1 0 0]);  % ListenerPosition
-    h(end+1)=plot3(x(1,4), x(1,5), x(1,6),'ro','MarkerFaceColor',[1 1 1]); % ListenerView
-    line([x(1,1), x(1,4)], [x(1,2) x(1,5)], [x(1,3) x(1,6)],'Color',[1 0 0]); % Line from LP to LV
-      % Listener: plot other appearances
-    for ii=2:size(x,1)
-      plot3(x(ii,1), x(ii,2), x(ii,3),'ro','MarkerFaceColor',[1 0 0]);  % ListenerPosition
-      plot3(x(ii,4), x(ii,5), x(ii,6),'ro','MarkerFaceColor',[1 1 1]); % ListenerView
-      line([x(ii,1), x(ii,4)], [x(ii,2) x(ii,5)], [x(ii,3) x(ii,6)],'Color',[1 0 0]); % Line from LP to LV
-    end
-      % Plot receivers (at the moment we consider the first ListenerPosition only)
-    if ndims(Obj.ReceiverPosition)>2
-      colors='rbgymc';
-      RP=shiftdim(Obj.ReceiverPosition(:,:,index),2);
-      h(end+1)=plot3(LP(1,1)+RP(:,1,1), LP(1,2)+RP(:,1,2), LP(1,3)+RP(:,1,3),'rx');
-      for ii=2:size(RP,2)
-        plot3(LP(1,1)+RP(:,ii,1), LP(1,2)+RP(:,ii,2), LP(1,3)+RP(:,ii,3),[colors(ii) 'x']);
-      end
-    else % single receivers
-      RP=Obj.ReceiverPosition;
-      h(end+1)=plot3(LP(1,1)+RP(:,1), LP(1,2)+RP(:,2), LP(1,3)+RP(:,3),'rx');
-    end
-      % Plot source
-    S=Obj.SourcePosition(index,:);
-    if strcmp(Obj.SourcePosition_Type,'spherical');
-      [X,Y,Z]=sph2cart(deg2rad(S(:,1)),deg2rad(S(:,2)),S(:,3));
+  case {'SimpleFreeFieldHRIR','SingleRoomDRIR'}
+    % Expand entries to the same number of measurement points
+    Obj = SOFAexpand(Obj);
+    % See if the room geometry is specified
+    if strcmp(Obj.GLOBAL_RoomType,'shoebox')
+        figure('Position',[1 1 (Obj.RoomCornerB(1)-Obj.RoomCornerA(1))*1.2 Obj.RoomCornerB(2)-Obj.RoomCornerA(2)]*100);
+        box on; hold on; h=[];
+        % plot the room
+        rectangle('Position',[Obj.RoomCornerA(1) ...  
+                              Obj.RoomCornerA(2) ...
+                              Obj.RoomCornerB(1)-Obj.RoomCornerA(1) ...
+                              Obj.RoomCornerB(2)-Obj.RoomCornerA(2)]);
     else
-      X=S(:,1); Y=S(:,2); Z=S(:,3);
+        figure; hold on;
     end
-    h(end+1)=plot3(X,Y,Z,'k.');
-    legend(h,{'ListenerPosition','ListenerView','Receivers','SourcePosition'});
-    xlabel(['X (in ' Obj.ListenerPosition_Units ')']);
-    ylabel(['Y (in ' Obj.ListenerPosition_Units ')']);
-    zlabel(['Z (in ' Obj.ListenerPosition_Units ')']);
-    title('SimpleFreeFieldHRIR');
-    
-%%    
-  case 'SingleRoomDRIR'    
-    Obj=SOFAexpand(Obj);
-    figure('Position',[1 1 (Obj.RoomCornerB(1)-Obj.RoomCornerA(1))*1.2 Obj.RoomCornerB(2)-Obj.RoomCornerA(2)]*100);
-    axis([Obj.RoomCornerA(1)-0.5 Obj.RoomCornerB(1)+0.5 Obj.RoomCornerA(2)-0.5 Obj.RoomCornerB(2)+0.5]);
-    box on; hold on; h=[];
-      % plot the room
-    rectangle('Position',[Obj.RoomCornerA(1) ...  
-                          Obj.RoomCornerA(2) ...
-                          Obj.RoomCornerB(1)-Obj.RoomCornerA(1) ...
-                          Obj.RoomCornerB(2)-Obj.RoomCornerA(2)]);
-      % plot listener and receivers
-    LP=Obj.ListenerPosition(index,:); LV=Obj.ListenerView(index,:);
-    RP=Obj.ReceiverPosition(index,:);
-    for ii=1:size(LP,1)
+    legendEntries = [];
+    title(sprintf('%s, %s',Obj.GLOBAL_SOFAConventions,Obj.GLOBAL_RoomType));
+    % Get ListenerPosition, ListenerView, ReceiverPosition, and SourcePosition
+    % NOTE: ListenerPosition is set to [0 0 0] for SimpleFreeFieldHRIR
+    LP = SOFAconvertCoordinates(Obj.ListenerPosition(index,:),Obj.ListenerPosition_Type,'cartesian');
+    LV = SOFAconvertCoordinates(Obj.ListenerView(index,:),Obj.ListenerView_Type,'cartesian');
+    RP = SOFAconvertCoordinates(Obj.ReceiverPosition(:,:,index),Obj.ReceiverPosition_Type,'cartesian');
+    S  = SOFAconvertCoordinates(Obj.SourcePosition(index,:),Obj.SourcePosition_Type,'cartesian');
+    % Use only unique listeber and source positons
+    uniquePoints = unique([LP LV S],'rows');
+    LP = uniquePoints(:,1:3);
+    LV = uniquePoints(:,4:6);
+    S  = uniquePoints(:,7:9);
+    % Plot ListenerPosition
+    legendEntries(end+1) = plot3(LP(:,1),LP(:,2),LP(:,3),'ro','MarkerFaceColor',[1 0 0]);
+    % Plot ListenerView
+    for ii=1:size(LV,1)
+      % Scale size of ListenerView vector smaller
+      LV(ii,:) = 0.2*LV(ii,:)./norm(LV(ii,:));
+      % Plot line for ListenerView vector
       line([LP(ii,1), LV(ii,1)+LP(ii,1)], [LP(ii,2) LV(ii,2)+LP(ii,2)], 'Color',[1 0 0]);
     end
-    h(end+1)=plot3(LP(:,1), LP(:,2), LP(:,3),'ro','MarkerFaceColor',[1 0 0]);
-    h(end+1)=plot3(LV(:,1)+LP(:,1), LV(:,2)+LP(:,2), LV(:,3)+LP(:,3),'ro','MarkerFaceColor',[1 1 1]);
-%     h(end+1)=plot3(LP(:,1)+RP(:,1), LP(:,2)+RP(:,2), ....
-%                     LP(:,3)+RP(:,3),'rx');
-    [lva,lve,lvr]=cart2sph(LV(ii,1),LV(ii,2),LV(ii,3));
-    [rpa,rpe,rpr]=cart2sph(RP(ii,1),RP(ii,2),RP(ii,3));
-    [x,y,z]=sph2cart(lva+rpa,lve+rpe,rpr);
-    plot3(LP(ii,1)+x, LP(ii,2)+y, LP(ii,3)+z,'rx')
-      % Plot source
-    S=Obj.SourcePosition(index,:);    
-    X=S(:,1); Y=S(:,2); Z=S(:,3);
-    h(end+1)=plot3(X,Y,Z,'b.');
-%     legend(h,{'ListenerPosition','ListenerView','Receivers','SourcePosition'});
-    legend(h,{'ListenerPosition','ListenerView','SourcePosition'});
-    title('SingleRoomDRIR');
+    legendEntries(end+1) = plot3(LV(:,1),LV(:,2),LV(:,3),'ro','MarkerFaceColor',[1 1 1]);
+    % Plot ReceiverPositon (this is plotted only for the first ListenerPosition)
+    RP = shiftdim(RP,2);
+    legendEntries(end+1) = plot3(LP(1,1)+RP(1,1,1), LP(1,2)+RP(1,1,2), LP(1,3)+RP(1,1,3),'ko');
+    for ii=2:size(RP,2)
+      plot3(LP(1,1)+RP(1,ii,1), LP(1,2)+RP(1,ii,2), LP(1,3)+RP(1,ii,3),'ko');
+    end
+    % Plot SourcePosition
+    legendEntries(end+1)=plot3(S(:,1),S(:,2),S(:,3),'k.');
+    legend(legendEntries,{'ListenerPosition','ListenerView','Receivers','SourcePosition'},'Location','NorthEastOutside');
+    xlabel(['X / ' Obj.ListenerPosition_Units]);
+    ylabel(['Y / ' Obj.ListenerPosition_Units]);
+    zlabel(['Z / ' Obj.ListenerPosition_Units]);
+
   otherwise
-    error('SOFAConventions not supported');
+    error('This SOFAConventions is not supported for plotting');
 end
+
+% Set fixed aspect ratio
+axis equal;
+% Add a little bit extra space at the axis
+axisLimits = axis();
+paddingSpace = 0.2 * max(abs(axisLimits(:)));
+axisLimits([1 3]) = axisLimits([1 3]) - paddingSpace;
+axisLimits([2 4]) = axisLimits([2 4]) + paddingSpace;
+axis(axisLimits);
