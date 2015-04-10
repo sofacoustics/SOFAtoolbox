20dfca1ef8caa271c814cec9397c6221dc1e3fc3
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-04-10 20:01:40 +0000

    Clean up SOFAload() code

--- API_MO/SOFAload.m
+++ API_MO/SOFAload.m
@@ -34,137 +34,156 @@ function Obj = SOFAload(fn,varargin)
 % Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing  permissions and limitations under the License. 
 
-%% sort input arguments
+
+%% ----- Input parameters ------------------------------------------------
 inputargs={}; % for setting flags with SOFAarghelper
-pDims=''; % for partial loading
+pDims='';     % for partial loading
 pDimRange=[]; % for partial loading
 jj=1; kk=1; ll=1;
 for ii=1:length(varargin)
-  if isnumeric(varargin{ii})
-    pDimRange(jj,:)=varargin{ii};
-    jj=jj+1;
-    if ii==1
-      inputargs{kk}=varargin{ii};
-      kk=kk+1;
+    if isnumeric(varargin{ii})
+        pDimRange(jj,:)=varargin{ii};
+        jj=jj+1;
+        if ii==1
+            inputargs{kk}=varargin{ii};
+            kk=kk+1;
+        end
+    elseif strfind('MREN',varargin{ii})
+        pDims(ll)=varargin{ii};
+        ll=ll+1;
+    elseif strcmp(varargin{ii},'nochecks')
+        inputargs{kk}=varargin{ii};
+    elseif strcmp(varargin{ii},'nodata')
+        inputargs{kk}=varargin{ii};
+        kk=kk+1;
     end
-  elseif strfind('MREN',varargin{ii})
-    pDims(ll)=varargin{ii};
-    ll=ll+1;
-  elseif strcmp(varargin{ii},'nochecks')
-    inputargs{kk}=varargin{ii};
-  elseif strcmp(varargin{ii},'nodata')
-    inputargs{kk}=varargin{ii};
-    kk=kk+1;
-  end
 end
 if ~isempty(pDimRange) && isempty(pDims)
-  pDims='M';
+    pDims='M';
 end
 
-%% set flags with SOFAarghelper
-definput.keyvals.Index=[];
-definput.flags.type={'data','nodata'};
-definput.flags.data={'checks','nochecks'};
-[flags,kv]=SOFAarghelper({'Index'},definput,inputargs);
+% Set flags with SOFAarghelper
+definput.keyvals.Index = [];
+definput.flags.type = {'data','nodata'};
+definput.flags.data = {'checks','nochecks'};
+[flags,kv] = SOFAarghelper({'Index'},definput,inputargs);
 
-%% check number of input arguments for partial loading
-if ~(length(pDims)==size(pDimRange,1) || isempty(kv.Index)), error('Missing dimension or range for partial loading'), end
-
-%% check file name
-fn=SOFAcheckFilename(fn);
-
-%% check if local or remote and download if necessary
+% Check number of input arguments for partial loading
+if ~(length(pDims)==size(pDimRange,1) || isempty(kv.Index))
+    error('Missing dimension or range for partial loading');
+end
+% Check file name
+fn = SOFAcheckFilename(fn);
+% Check if local or remote and download if necessary
 if strfind(fn,'://')
-  % remote path: download as temporary
-  newfn=[tempname '.sofa'];
-  urlwrite(fn, newfn);
+    % remote path: download as temporary
+    newfn = [tempname '.sofa'];
+    urlwrite(fn, newfn);
 else
-  newfn=fn;
-  if ~exist(fn,'file') % file does not exist? 
-    warning(['File not found: ' fn]);
-      % local path: replace SOFAdbPath by SOFAdbURL, download to SOFAdbPath 
-    if length(fn)>length(SOFAdbPath) % fn is longer than SOFAdbPath?
-      if strcmp(SOFAdbPath,fn(1:length(SOFAdbPath))) % fn begins with SOFAdbPath
-          % create dir if not existing
-        if ~exist(fileparts(newfn),'dir'), 
-          [success,msg]=mkdir(fileparts(newfn));
-          if success~=1, error(msg); end
-        end          
-        webfn=fn(length(SOFAdbPath)+1:end);
-        webfn(strfind(webfn,'\'))='/';
-        webfn=[SOFAdbURL regexprep(webfn,' ','%20')];        
-        disp(['Downloading ' fn(length(SOFAdbPath)+1:end) ' from ' SOFAdbURL]);
-        [f,stat]=urlwrite(webfn,fn);
-        if ~stat
-          error(['Could not download file: ' webfn]);
+    newfn = fn;
+    if ~exist(fn,'file') % file does not exist? 
+        warning(['File not found: ' fn]);
+        % local path: replace SOFAdbPath by SOFAdbURL, download to SOFAdbPath 
+        if length(fn)>length(SOFAdbPath) % fn is longer than SOFAdbPath?
+            if strcmp(SOFAdbPath,fn(1:length(SOFAdbPath))) % fn begins with SOFAdbPath
+                % create dir if not existing
+                if ~exist(fileparts(newfn),'dir'), 
+                    [success,msg] = mkdir(fileparts(newfn));
+                    if success~=1, error(msg); end
+                end          
+            webfn = fn(length(SOFAdbPath)+1:end);
+            webfn(strfind(webfn,'\'))='/';
+            webfn = [SOFAdbURL regexprep(webfn,' ','%20')];        
+            disp(['Downloading ' fn(length(SOFAdbPath)+1:end) ' from ' SOFAdbURL]);
+            [f,stat] = urlwrite(webfn,fn);
+            if ~stat
+                error(['Could not download file: ' webfn]);
+            end
+            else % fn not existing and not beginning with SOFAdbPath --> error
+                error(['Unable to read file ''' fn ''': no such file']);
+            end
+        else % fn not existing and shorter than SOFAdbPath --> error
+            error(['Unable to read file ''' fn ''': no such file']);
         end
-      else % fn not existing and not beginning with SOFAdbPath --> error
-        error(['Unable to read file ''' fn ''': no such file']);
-      end
-    else % fn not existing and shorter than SOFAdbPath --> error
-        error(['Unable to read file ''' fn ''': no such file']);
     end
-  end
 end 
 
-%% Load the object
-if flags.do_nodata, Obj=NETCDFload(newfn,'nodata'); end;
-if flags.do_data, 
-  try
-    if isempty(kv.Index),
-        Obj=NETCDFload(newfn,'all');
-    else
-      [Obj]=NETCDFload(newfn,pDimRange,pDims);
+
+%% ----- Load SOFA file --------------------------------------------------
+if flags.do_nodata
+    Obj = NETCDFload(newfn,'nodata');
+elseif flags.do_data
+    try
+        if isempty(kv.Index),
+            Obj = NETCDFload(newfn,'all');
+        else
+            Obj = NETCDFload(newfn,pDimRange,pDims);
+        end
+    catch 
+        error(['Error loading the file: ' newfn]);
     end
-  catch 
-    error(['Error loading the file: ' newfn]);
-  end
 end
 
-%% Return if no checks should be performed
-if flags.do_nochecks, return; end;
 
-%% Check if SOFA conventions
-if ~isfield(Obj,'GLOBAL_Conventions'), error('File is not a valid SOFA file'); end
-if ~strcmp(Obj.GLOBAL_Conventions,'SOFA'), error('File is not a valid SOFA file'); end
-if ~isfield(Obj,'GLOBAL_SOFAConventions'), error('Information about SOFA conventions is missing'); end
-try
-	X=SOFAgetConventions(Obj.GLOBAL_SOFAConventions,'m');
-catch
-	error('Unsupported SOFA conventions');
-end
+%% ----- Checking of loaded data -----------------------------------------
+% Return if no checks should be performed
+if flags.do_nochecks
+    return;
+else
 
-%% Check if DataType is present
-if ~isfield(Obj,'GLOBAL_DataType'), error('DataType is missing'); end
-%if ~strcmp(Obj.GLOBAL_DataType,X.GLOBAL_DataType), error('Wrong DataType'); end remove because this might be corrected in upgradeconventions
-
-%% Ensure backwards compatibility
-modified=1;
-while modified, [Obj,modified]=SOFAupgradeConventions(Obj); end
-
-%% If data loaded, check if correct data format
-if ~flags.do_nodata,
-	if ~isfield(Obj,'Data'), error('Data is missing'); end
-	f=fieldnames(X.Data);
-	for ii=1:length(f)
-    if ~isfield(Obj.Data,f{ii})
-      Obj.Data.(f{ii})=X.Data.(f{ii});
-      Obj.API.Dimensions.Data.(f{ii})=X.API.Dimensions.Data.(f{ii});
-      warning(['Data.' f{ii} ' was missing, set to default']);
+    % ----- Check for SOFA conventions -----
+    if ~isfield(Obj,'GLOBAL_Conventions') || ~strcmp(Obj.GLOBAL_Conventions,'SOFA')
+        error('File is not a valid SOFA file');
     end
-	end
-	Obj=SOFAupdateDimensions(Obj);
-end
-
-%% Check if mandatory variables are present
-f=fieldnames(X);
-for ii=1:length(f)
-	if ~isfield(Obj,f{ii})
-    Obj.(f{ii})=X.(f{ii});
-    if ~strfind(f{ii},'_'),
-      Obj.API.Dimensions.(f{ii})=X.API.Dimensions.(f{ii});
+    if ~isfield(Obj,'GLOBAL_SOFAConventions')
+        error('Information about SOFA conventions is missing');
     end
-    warning([f{ii} ' was missing, set to default']);
-  end
-end
+    try
+        ObjTemplate = SOFAgetConventions(Obj.GLOBAL_SOFAConventions,'m');
+    catch
+        error('Unsupported SOFA conventions');
+    end
 
+    % ----- Check if DataType is present -----
+    if ~isfield(Obj,'GLOBAL_DataType')
+        error('DataType is missing');
+    end
+
+    % ----- Check if mandatory variables are present -----
+    for field = fieldnames(ObjTemplate)'
+        field = field{:};
+        if ~isfield(Obj,field)
+            Obj.(field) = ObjTemplate.(field);
+            if ~strfind(field,'_') % if `_` is missing it is a dimension
+                Obj.API.Dimensions.(field) = ObjTemplate.API.Dimensions.(field);
+            end
+            warning([field ' was missing, set to default']);
+        end
+    end
+
+    % ---- If data loaded, check for correct data format -----
+    if ~flags.do_nodata
+        if ~isfield(Obj,'Data')
+            error('Data is missing');
+        end
+        for field = fieldnames(ObjTemplate.Data)'
+            field = field{:};
+            if ~isfield(Obj.Data,field)
+                Obj.Data.(field) = ObjTemplate.Data.(field);
+                Obj.API.Dimensions.Data.(field) = ...
+                    ObjTemplate.API.Dimensions.Data.(field);
+                warning(['Data.' field ' was missing, set to default']);
+            end
+        end
+        Obj = SOFAupdateDimensions(Obj);
+    end
+
+    % ----- Ensure backwards compatibility -----
+    % TODO: is the while loop necessary, or could be handled just by calling
+    % SOFAupgradeConventions(Obj) once?
+    modified = 1;
+    while modified
+        [Obj,modified] = SOFAupgradeConventions(Obj);
+    end
+
+end
