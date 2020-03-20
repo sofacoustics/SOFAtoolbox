d5055728557baf775055f7c8a3a76bff623106b0
isfmiho <michael.mihocic@oeaw.ac.at>
2020-03-20 10:56:38 +0100

    Update SOFAconvertTUBerlinBRIR2SOFA.m
    bug fixed in Obj.ReceiverPosition: left / right was inverted

--- API_MO/converters/SOFAconvertTUBerlinBRIR2SOFA.m
+++ API_MO/converters/SOFAconvertTUBerlinBRIR2SOFA.m
@@ -6,7 +6,7 @@ function Obj=SOFAconvertTUBerlinBRIR2SOFA(irs)
 
 % SOFA API - demo script
 % Copyright (C) 2012-2013 Acoustics Research Institute - Austrian Academy of Sciences
-% Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "License")
+% Licensed under the EUPL, Version 1.1 or â€“ as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "License")
 % You may not use this work except in compliance with the License.
 % You may obtain a copy of the License at: http://joinup.ec.europa.eu/software/page/eupl
 % Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,7 +60,7 @@ Obj.ListenerPosition = irs.head_position';
 Obj.ListenerView = [x,y,z];
 Obj.ListenerUp = [0 0 1];
 % Receiver position for a dummy head (imported from SimpleFreeFieldHRIR)
-Obj.ReceiverPosition = [0,-0.09,0; 0,0.09,0];
+Obj.ReceiverPosition = [0,0.09,0; 0,-0.09,0];
   
 %% Update dimensions
 Obj=SOFAupdateDimensions(Obj);
