874884098abfcf577aec9338b97517adff47141a
Piotr Majdak <piotr@majdak.com>
2020-07-24 20:12:59 +0200

    calculation of SH coefficients and a demo for HRTF in SH added.

---
+++ API_MO/coordinates/sph2SH.m
@@ -0,0 +1,46 @@
+function [S, N_SH] = sph2SH(dirs, L)
+%S = sph2SH(dirs, N);
+%  sph2SH calculates real-valued spherical harmonics S_l,m
+%  for directions dirs = [azi ele] (in degrees) up to order L. 
+%  [S, N_SH] = sph2SH(dirs, N); returns the number of SH coefficients.
+% 
+% 20200724 - Piotr Majdak, adapted from getSH.m from https://github.com/polarch/Spherical-Harmonic-Transform
+
+  N_dirs = size(dirs, 1); % number of discrete positions
+  N_SH = (L+1)^2;         % number of SH coefficients
+  dirs=deg2rad(dirs);			% convert to radiant
+  dirs(:,2) = pi/2 - dirs(:,2); % convert to zenith angle
+
+  S = zeros(N_SH, N_dirs);% matrix with the coefficients
+
+	  % n = 0, direction-independent component
+	Llm = legendre(0, cos(dirs(:,2)'));
+	Nlm = sqrt(1./(4*pi)) * ones(1,N_dirs);
+	CosSin = zeros(1,N_dirs);
+	CosSin(1,:) = ones(1,size(dirs,1));
+	S(1, :) = Nlm .* Llm .* CosSin;
+	
+	  % n > 0, direction-dependent components
+	idx = 1;
+  for l=1:L
+
+    m = (0:l)';
+
+    Llm = legendre(l, cos(dirs(:,2)'));
+    condon = (-1).^[m(end:-1:2);m] * ones(1,N_dirs);
+    Llm = condon .* [Llm(end:-1:2, :); Llm];
+
+    mag = sqrt( (2*l+1)*factorial(l-m) ./ (4*pi*factorial(l+m)) );
+      % create the ACN ordering: [m<0; m=0; m>0]
+    Nlm = [mag(end:-1:2) * ones(1,N_dirs); mag * ones(1,N_dirs)]; 
+
+    CosSin = zeros(2*l+1,N_dirs);
+    CosSin(l+1,:) = ones(1,size(dirs,1)); % m=0
+    CosSin(m(2:end)+l+1,:) = sqrt(2)*cos(m(2:end)*dirs(:,1)'); % m>0
+    CosSin(-m(end:-1:2)+l+1,:) = sqrt(2)*sin(m(end:-1:2)*dirs(:,1)'); % m<0
+
+    S(idx+1:idx+(2*l+1), :) = Nlm .* Llm .* CosSin;
+    idx = idx + 2*l+1;
+  end
+    
+  S = S.';
