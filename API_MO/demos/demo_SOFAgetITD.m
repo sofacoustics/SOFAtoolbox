cea420e94478c8a042c902f73d48f7cf06b4003b
Elephant ~ <r.davicarvalho@gmail.com>
2021-10-05 10:15:46 -0300

    Include recent updates in main branch for SOFAplotHRTF.m

--- API_MO/demos/demo_SOFAgetITD.m
+++ API_MO/demos/demo_SOFAgetITD.m
@@ -17,7 +17,7 @@ SOFAfn=fullfile(SOFAdbPath, 'database', 'cipic', ['subject_' sprintf('%03d',subj
 Obj=SOFAload(SOFAfn, 'nochecks');
 
 %% Calculate Interaural time delay
-[itd_time, Obj_time] = SOFAgetITD(Obj, 'time', 'thr', 20, 'upsample', 16);
+[itd_time, Obj_time] = SOFAgetITD(Obj, 'time', 'thr', 20);
 [itd_samples, Obj_samples] = SOFAgetITD(Obj, 'samples', 'thr', 20);
 
 %% Plot results
@@ -35,4 +35,5 @@ ylabel(['Samples (Fs:' num2str(Obj.Data.SamplingRate), 'Hz)'])
 axis tight
 
 %%% Polar plot 
+figure()
 SOFAplotHRTF(Obj, 'itdhorizontal');
